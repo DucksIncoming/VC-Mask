ebdc0b89bdd2c0049a1035611436ef0996867423
DucksIncoming <ducksincoming@gmail.com>
2022-06-08 15:55:06 -0400

    fine whatever idc

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import numpy as np
 audio = pyaudio.PyAudio()
 BUFFER = 1024 * 4
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
 
 DebugMode = True
 
+def defineFormants():
+    pass
 
 def getMicAudio():
     global DebugMode
@@ -21,28 +23,18 @@ def getMicAudio():
         rate=RATE,
         input=True,
         output=True,
-        frames_per_buffer=BUFFER
+        frames_per_buffer=BUFFER,
+        start=True
     )
-
-    fig, ax = plt.subplots()
-    x = np.arange(0, 2 * BUFFER, 2)
-    line, = ax.plot(x, np.random.rand(BUFFER))
-    ax.set_xlim(0,255)
-    ax.set_ylim(0, BUFFER)
 
     try:
         while True:
-            data = stream.read(BUFFER)
-            data_int = np.array(struct.unpack(str(4 * BUFFER) + 'B', data), dtype='b')[::2] + 127
-
-            line.set_ydata(data_int)
-            fig.canvas.draw()
-            fig.canvas.flush_events()
+            pass
     except KeyboardInterrupt:
         pass
 
     stream.close()
-    print("\n> Closing audio...")
-
+    print("\n> Closing Audio...")
+        
 print("Listening...")
 getMicAudio()

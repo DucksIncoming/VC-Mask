b67dd903e1e5ceacb7e44038a53c881b1d7b5617
DucksIncoming <ducksincoming@gmail.com>
2022-06-08 17:09:47 -0400

    idfk

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import pyaudio
 import struct
+import wave
 import matplotlib.pyplot as plt
 import numpy as np
 
 audio = pyaudio.PyAudio()
-BUFFER = 1024 * 4
+BUFFER = 1
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 
 DebugMode = True
 
-def defineFormants():
-    pass
+def pitchShift(audioIn, amount: float):
+    audioIn = np.array(wave.struct.unpack("%dh"(len(audioIn) / amount)))
+
+    audioIn = np.fft.rfft(audioIn)
+    audioIn = np.roll(audioIn, amount)
+    audioIn[0:amount] = 0
+    audioIn = np.fft.irfft(audioIn)
+
+    return audioIn
+
+def formantShift(audioIn, gender: str):
+    return audioIn
 
 def getMicAudio():
     global DebugMode
+    global audio
 
     stream = audio.open(
         format=FORMAT,
@@ -29,7 +41,11 @@ def getMicAudio():
 
     try:
         while True:
-            pass
+            data = stream.read(BUFFER)
+
+            data = pitchShift(data, 100)
+            #data = formantShift(data, "female")
+            stream.write(data)
     except KeyboardInterrupt:
         pass
 

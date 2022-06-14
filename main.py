243472f9c8c0070b7aa89259443b1d7c42eac340
DucksIncoming <ducksincoming@gmail.com>
2022-06-14 04:19:54 -0400

    pitch shift works yayaya

--- main.py
+++ main.py
@@ -1,56 +1,45 @@
-import pyaudio
-import struct
-import wave
-import matplotlib.pyplot as plt
-import numpy as np
+# Good amount of pitch-shifting code is stolen from PRABLANC P. because I didn't wanna do it myself.
+# https://github.com/pprablanc/ppsrt
 
-audio = pyaudio.PyAudio()
-BUFFER = 1
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
+from pyaudio import PyAudio, paContinue, paFloat32
+import readchar
+import AudioEffect
 
-DebugMode = True
+PITCH = 2.0
+IN = []
+OUT = []
 
-def pitchShift(audioIn, amount: float):
-    audioIn = np.array(wave.struct.unpack("%dh"(len(audioIn) / amount)))
+def main():
+    global PITCH
 
-    audioIn = np.fft.rfft(audioIn)
-    audioIn = np.roll(audioIn, amount)
-    audioIn[0:amount] = 0
-    audioIn = np.fft.irfft(audioIn)
+    effect = AudioEffect.ProsodicModificationRealTime(44100, 'float32')
+    effect.pitch_rate = PITCH
+    BUFFER_PYAUDIO_SIZE = effect.mid_buffer_size
 
-    return audioIn
+    def callback(in_data, frame_count, time_info, flag):
+        global PITCH, IN, OUT
+        in_data = effect.str2numpy(in_data)
+        IN.append(in_data)
+        out_data = effect.pitchshifting(in_data)
+        OUT.append(out_data)
+        out_data = effect.numpy2str(out_data)
+        return out_data, paContinue
 
-def formantShift(audioIn, gender: str):
-    return audioIn
+    pa = PyAudio()
+    stream = pa.open(format = paFloat32,
+                     channels = 1,
+                     rate = 44100,
+                     frames_per_buffer = BUFFER_PYAUDIO_SIZE,
+                     input = True,
+                     output = True,
+                     stream_callback = callback)
 
-def getMicAudio():
-    global DebugMode
-    global audio
+    print("Listening...")
+    keypress = readchar.readchar()
 
-    stream = audio.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        output=True,
-        frames_per_buffer=BUFFER,
-        start=True
-    )
-
-    try:
-        while True:
-            data = stream.read(BUFFER)
-
-            data = pitchShift(data, 100)
-            #data = formantShift(data, "female")
-            stream.write(data)
-    except KeyboardInterrupt:
-        pass
-
+    print("Closing Audio...")
     stream.close()
-    print("\n> Closing Audio...")
-        
-print("Listening...")
-getMicAudio()
+    pa.terminate()
+
+if __name__ == "__main__":
+    main()
